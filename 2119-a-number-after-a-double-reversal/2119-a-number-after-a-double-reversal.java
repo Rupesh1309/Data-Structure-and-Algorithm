061ad3c0a6cdc91a2cc9dbf7491d21915446155e
Rupesh Yadav <ruppeshyadav7317@gmail.com>
2026-09-12 23:21:53 +0530

    Time: 0 ms (100%), Space: 42.2 MB (28.47%) - LeetHub

---
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isSameAfterReversals(int num) {
+        if(num%10==0){
+            if(num==0) return true;
+            else return false;
+        }
+        return true;
+    }
+}
